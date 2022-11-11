8484e2eef9d884ac7d55f7071d0994cc433f2282
Brad <bycarlson23@gmail.com>
2022-11-10 22:45:23 -0600

    setting up server.js on server side to work w apollo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+// bring in apollo server
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// bring in typedefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// set up apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +26,19 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// creating instance of apollo server w graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// calling the async function to start server
+startApolloServer(typeDefs, resolvers);
